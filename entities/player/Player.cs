5eaa6748373a9d0bae8408b3ff808eba4be516cf
Brage Tiller Naustan <bragetn@stud.ntnu.no>
2025-02-14 12:04:29 +0100

    Make player menu toggleable with controller

--- entities/player/Player.cs
+++ entities/player/Player.cs
@@ -6,13 +6,19 @@ public partial class Player : XROrigin3D
 {
     private XRCamera3D _camera;
     private XRController3D _eyes;
+    private XRController3D _leftHand;
     private Node3D _target;
+    private Node3D _playerMenu;
     
     public override void _Ready()
     {
         _camera = GetNode<XRCamera3D>("XRCamera3D");
         _eyes = GetNode<XRController3D>("Eyes");
+        _leftHand = GetNode<XRController3D>("LeftHand");
         _target = GetNode<Node3D>("Eyes/MeshInstance3D");
+        _playerMenu = GetNode<Node3D>("LeftHand/PlayerMenu");
+        
+        _leftHand.ButtonPressed += LeftHandButtonPressed;
     }
 
     public override void _PhysicsProcess(double delta)
@@ -32,8 +38,25 @@ public partial class Player : XROrigin3D
         }
     }
 
-    private void _on_xr_tools_hold_button_pressed()
+    private void LeftHandButtonPressed(string name)
     {
-        GD.Print("funker");
+        if (name == "ax_button")
+        {
+            if (_playerMenu.ProcessMode == ProcessModeEnum.Inherit)
+            {
+                _playerMenu.ProcessMode = ProcessModeEnum.Disabled;
+                _playerMenu.Visible = false;
+            }
+            else
+            {
+                _playerMenu.ProcessMode = ProcessModeEnum.Inherit;
+                _playerMenu.Visible = true;
+            }
+        }
+        else
+        {
+            // For the future generations
+            return;
+        }
     }
 }
