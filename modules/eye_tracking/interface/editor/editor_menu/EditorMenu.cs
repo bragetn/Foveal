4ee7d5a55f78b3eb875b703063433d6f3d0d975b
Brage Tiller Naustan <bragetn@stud.ntnu.no>
2025-04-04 11:42:05 +0200

    Update menu icons

---
+++ modules/eye_tracking/interface/editor/editor_menu/EditorMenu.cs
@@ -0,0 +1,88 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public partial class EditorMenu : Control
+{
+    private PackedScene _defaultPackedScene = GD.Load<PackedScene>("uid://bo5e8fm3iq5bk");
+    private PackedScene _saveAsPackedScene = GD.Load<PackedScene>("uid://bm3fu54guqawa");
+    private PackedScene _loadTestPackedScene = GD.Load<PackedScene>("uid://bed0c47cubn6q");
+    private PackedScene _confirmationPackedScene = GD.Load<PackedScene>("uid://bxtuspc7tq5ja"); 
+    
+    private Button _newButton;
+    private Button _openButton;
+    private Button _saveButton;
+    private Button _saveAsButton;
+    private Button _mainMenuButton;
+    private Panel _sidePanel;
+
+    private Node _currentScene;
+
+    public override void _Ready()
+    {
+        _newButton = GetNode<Button>("HBoxContainer/ButtonPanel/MarginContainer/VBoxContainer/NewButton");
+        _openButton = GetNode<Button>("HBoxContainer/ButtonPanel/MarginContainer/VBoxContainer/OpenButton");
+        _saveButton = GetNode<Button>("HBoxContainer/ButtonPanel/MarginContainer/VBoxContainer/SaveButton");
+        _saveAsButton = GetNode<Button>("HBoxContainer/ButtonPanel/MarginContainer/VBoxContainer/SaveAsButton");
+        _mainMenuButton = GetNode<Button>("HBoxContainer/ButtonPanel/MarginContainer/VBoxContainer/MainMenuButton");
+        _sidePanel = GetNode<Panel>("HBoxContainer/SidePanel");
+
+        _newButton.Pressed += NewTest;
+        _openButton.Pressed += OpenTest;
+        _saveButton.Pressed += SaveTest;
+        _saveAsButton.Pressed += SaveAsTest;
+        _mainMenuButton.Pressed += ExitToMainMenu;
+    }
+
+    private void NewTest()
+    {
+        SetSidePanel(_confirmationPackedScene);
+
+        if (_currentScene is ConfirmationMenu confirmationMenu)
+        {
+            confirmationMenu.SetPrompt("Er du sikker?");
+            confirmationMenu.ConfirmationYes += () =>
+            {
+                EyeTrackingRadio.Instance.EmitSignal("ClearTargets");
+                SetSidePanel(_defaultPackedScene);
+            };
+            confirmationMenu.ConfirmationNo += () => SetSidePanel(_defaultPackedScene);
+        }
+    }
+
+    private void OpenTest()
+    {
+        SetSidePanel(_loadTestPackedScene);
+    }
+    
+    private void SaveTest()
+    {
+        EyeTrackingRadio.Instance.EmitSignal("SaveTestFile", "");
+    }
+
+    private void SaveAsTest()
+    {
+        SetSidePanel(_saveAsPackedScene);
+    }
+
+    private void ExitToMainMenu()
+    {
+        SetSidePanel(_confirmationPackedScene);
+
+        if (_currentScene is ConfirmationMenu confirmationMenu)
+        {
+            confirmationMenu.SetPrompt("Er du sikker?");
+            confirmationMenu.ConfirmationYes += () => CoreRadio.Instance.EmitSignal("LoadScene", "uid://bcskthtw74py2");
+            confirmationMenu.ConfirmationNo += () => SetSidePanel(_defaultPackedScene);
+        }
+    }
+
+    private void SetSidePanel(PackedScene newSidePanelMenu)
+    {
+        _currentScene?.QueueFree();
+        _currentScene = newSidePanelMenu.Instantiate();
+        _sidePanel.AddChild(_currentScene);
+    }
+}
